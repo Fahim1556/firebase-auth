158dab16eb89c63bc35dcbfce8953710b463e196
Fahim1556 <49098556+Fahim1556@users.noreply.github.com>
2020-09-09 00:08:24 +0600

    first commit

---
+++ src/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCBwbIny-UkQXIE-PtZQY-TmJ0dV1nkhl4",
+    authDomain: "ema-john-simple-c0f63.firebaseapp.com",
+    databaseURL: "https://ema-john-simple-c0f63.firebaseio.com",
+    projectId: "ema-john-simple-c0f63",
+    storageBucket: "ema-john-simple-c0f63.appspot.com",
+    messagingSenderId: "875753041701",
+    appId: "1:875753041701:web:1edc94bffd15b4ae0fb4cc"
+  };
+
+  export default firebaseConfig ;
